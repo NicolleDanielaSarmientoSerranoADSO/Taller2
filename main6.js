6b1d260f18e180bca20226fcfb94a13815e399a6
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-10-11 08:54:25 -0500

    Ejer6

---
+++ main6.js
@@ -0,0 +1,8 @@
+addEventListener("DOMContentLoaded",()=>{
+      // 6. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg
+    // transporta ?
+    document.write(`26. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg transporta ?<br>`)
+    let cajas=25,peso=748,pesoT=0
+    pesoT=(cajas*peso)
+    document.write(`El peso total que transporta el camion es de: ${pesoT}`)
+})
